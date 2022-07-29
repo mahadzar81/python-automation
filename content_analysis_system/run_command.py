d6e6a8cf79465c8410538964c5ba9588210588ce
Mahadzar Tajuddin <mahadzar.karsof@gmail.com>
2022-07-29 16:37:33 +0000

    Fix typo error

--- content_analysis_system/run_command.py
+++ content_analysis_system/run_command.py
@@ -113,7 +113,7 @@ def run_command(host):
     remote_conn_pre = paramiko.SSHClient()
     remote_conn_pre.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
-    remote_conn_pre.connect(host, username=username, password=password, look_for_keys=False, allow_agent=False, timeout=10)
+    remote_conn_pre.connect(host, username=username, password=credential, look_for_keys=False, allow_agent=False, timeout=10)
     print "SSH connection established to %s" % host
     remote_conn = remote_conn_pre.invoke_shell()
     print "SSH session established"
